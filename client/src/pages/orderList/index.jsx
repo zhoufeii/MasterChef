350139d5ac5a2a3fe6a85a403ec3c3a8ee9dfa8e
zhoufei <dengyu@microants.cn>
2020-03-21 14:55:18 +0800

    feat:完成我的订单页面

--- client/src/pages/orderList/index.jsx
+++ client/src/pages/orderList/index.jsx
@@ -1,9 +1,16 @@
 import "./index.less";
 
+import moment from "moment";
+import { AtActivityIndicator } from "taro-ui";
+
 import { View } from "@tarojs/components";
 import { Component } from "@tarojs/taro";
 
 import { getGlobalData } from "../../utils/globalData";
+
+function formatTime(date) {
+    return date.format('YYYY-MM-DD HH:mm')
+}
 
 export default class Index extends Component {
 
@@ -13,7 +20,9 @@ export default class Index extends Component {
             env: getGlobalData('env') || '',
             pageNo: 0,
             pageSize: 10,
-            noMore: false
+            noMore: false,
+            loading: true,
+            initialCompleted: false
         }
     }
 
@@ -31,10 +40,11 @@ export default class Index extends Component {
 
     onReachBottom() {
         console.log("上拉触底事件")
-        const { pageNo, pageSize, noMore = false } = this.state;
+        const { pageNo, noMore = false } = this.state;
         if (!noMore) {
             this.setState({
-                pageNo: +pageNo + 1
+                pageNo: +pageNo + 1,
+                loading: true
             }, this.handleGetOrderList)
         }
     }
@@ -44,15 +54,17 @@ export default class Index extends Component {
     }
 
     handleGetOrderList = () => {
-        const { env, pageNo, pageSize, list = [], noMore = false } = this.state;
+        const { env, pageNo, pageSize, list = [] } = this.state;
         Taro.cloud.callFunction({
             name: 'orders',
             data: { action: 'getOrders', pageNo, pageSize, env },
         }).then(res => {
             console.log(res)
             this.setState({
+                loading: false,
                 list: list.concat(res.result.data || []),
-                noMore: res.result.data && res.result.data.length < pageSize
+                noMore: res.result.data && res.result.data.length < pageSize,
+                initialCompleted: true
             })
         }).catch(err => {
             showToast('获取订单失败，请联系大熊！')
@@ -66,37 +78,75 @@ export default class Index extends Component {
     }
 
     render() {
-        const { list = [] } = this.state;
+        const { list = [], loading = true, initialCompleted } = this.state;
         return (
             <View className='orderList'>
                 {
-                    list.length ? <View>
+                    loading ? <View className='modal_wrapper'>
+                        <AtActivityIndicator content='加载中...'></AtActivityIndicator>
+                    </View> : null
+                }
+                {
+                    initialCompleted && list.length ? <View>
                         {
                             list.map(orderItem => {
                                 return <View key={orderItem._id} className='order_item_wrapper'>
                                     <View className='order_item_head'>
-                                        <View>熊家厨房【官方直营】</View>
+                                        <View className='shop_name'>熊家厨房【官方直营】</View>
+                                        <View className='right_arrow'>
+                                            <Image src='https://wecip.oss-cn-hangzhou.aliyuncs.com/masterChef/common_icon/right_arrow.png' />
+                                        </View>
                                     </View>
                                     <View className='order_item_mid'>
-                                        <View className='food_pic_wrapper'>
-                                            {
-                                                orderItem.list.map(item => {
-                                                    return <Image className='food_pic' key={item.id} src={item.pic} />
-                                                })
-                                            }
+                                        <View className='foods_wrapper'>
+                                            <ScrollView
+                                                className='food_pic_wrapper'
+                                                scrollX
+                                                scrollAnchoring={true}
+                                                scrollWithAnimation={true}
+                                                scrollTop={0}
+                                            >
+                                                {
+                                                    orderItem.list.map(item => {
+                                                        return <Image className='food_pic' key={item.id} src={item.pic} />
+                                                    })
+                                                }
+                                            </ScrollView>
                                         </View>
+
                                         <View className='food_count'>{`共${orderItem.list.length}道菜`}</View>
                                     </View>
                                     <View className='order_item_bottom'>
-                                        <View>评价</View>
+                                        <View className='order_item_bottom_item'>
+                                            <View className='bottom_item_label'>下单时间</View>
+                                            <View className='bottom_item_content'>{formatTime(moment(orderItem.createTime)) || '-'}</View>
+                                        </View>
+                                        <View className='order_item_bottom_item'>
+                                            <View className='bottom_item_label'>送达时间</View>
+                                            <View className='bottom_item_content'>{orderItem.deliverDate || '-'}</View>
+                                        </View>
+                                        {/* <View className='order_item_bottom_item'>
+                                            <View></View>
+                                            <View className='right_btns'>
+                                                <View className='same_more_btn'>再来一顿</View>
+                                                <View className='comment_btn'>评价</View>
+                                            </View>
+                                        </View> */}
                                     </View>
                                 </View>
                             })
                         }
-                    </View> : <View>还没有点过菜</View>
+                    </View> : null
                 }
                 {
-                    noMore ? <View className='bottom_line'>我也是有底线的</View> : null
+                    initialCompleted && !list.length ? <View>还没有点过菜</View> : null
+                }
+                {
+                    noMore ? <View className='bottom_line_wrapper'>
+                        <View className='bottom_line'></View>
+                        <View className='bottom_line_text'>我也是有底线的</View>
+                        <View className='bottom_line'></View>
+                    </View> : null
                 }
             </View>
         )
